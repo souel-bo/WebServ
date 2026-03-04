47000670186b053fe04c998870e548775fc3a6fd
ACH4Q <achaqmohammedamine1@gmail.com>
2026-03-04 02:11:52 +0000

    Router algo for paths

--- srcs/Router.cpp
+++ srcs/Router.cpp
@@ -19,3 +19,62 @@ std::string Router::joinPaths(const std::string& path1, const std::string& path2
     return path1 + path2;
 }
 
+RouteResult Router::resolve(const HttpRequest& req, const ServerConfig& config)
+{
+    RouteResult result;
+    result.isAllowed = true;
+    std::string uri = req.getPath();
+    const Location* bestMatch = NULL;
+    size_t longestMatchLength = 0;
+    for (size_t i = 0; i < config.locations.size(); ++i)
+    {
+        const std::string& locPath = config.locations[i].path;
+        if (uri.find(locPath) == 0)
+        {
+            if (locPath.length() > longestMatchLength)
+            {
+                longestMatchLength = locPath.length();
+                bestMatch = &config.locations[i];
+            }
+        }
+    }
+    if (bestMatch != NULL)
+    {
+        result.location = *bestMatch;
+        bool methodFound = false;
+        if (bestMatch->methods.empty())
+        {
+            methodFound = true;
+        } else
+        {
+            for (size_t i = 0; i < bestMatch->methods.size(); ++i)
+            {
+                if (bestMatch->methods[i] == req.getMethod())
+                {
+                    methodFound = true;
+                    break;
+                }
+            }
+        }
+        if (!methodFound)
+        {
+            result.isAllowed = false;
+        }
+        if (!bestMatch->alias.empty())
+        {
+            std::string remainingUri = uri.substr(longestMatchLength);
+            result.finalPath = joinPaths(bestMatch->alias, remainingUri);
+        } else if (!bestMatch->root.empty())
+        {
+            result.finalPath = joinPaths(bestMatch->root, uri);
+        } else
+        {
+            result.finalPath = joinPaths(config.root, uri);
+        }
+    } 
+    else
+    {
+        result.finalPath = joinPaths(config.root, uri);
+    }
+    return result;
+}
