40b9f3a2cd9ab8835fad045976a0a2423cfcb14f
Achaq Mohammed Amine <138736773+ACH4Q@users.noreply.github.com>
2026-03-18 15:28:26 +0000

    .conf

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,20 +6,57 @@
 #include "Event.hpp"
 
 
+bool hasConfExtension(const std::string& filepath) 
+{
+    size_t slashPos = filepath.find_last_of('/');
+    std::string filename = (slashPos == std::string::npos) ? filepath : filepath.substr(slashPos + 1);
+    if (filename.length() < 6) 
+        return false;
+    if (filename.substr(filename.length() - 5) != ".conf")
+        return false;
+    int dotCount = 0;
+    for (size_t i = 0; i < filename.length(); i++) 
+    {
+        if (filename[i] == '.') 
+            dotCount++;
+    }
+    if (dotCount > 1) 
+        return false;
+
+    return true;
+}
+
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    std::string configFile;
+    if (argc == 1) 
     {
-        std::cerr << "Usage: ./webserv [config_file]" << std::endl;
+        std::cout << "No config file provided. Defaulting to 'webserv.conf'..." << std::endl;
+        configFile = "webserv.conf"; 
+    } 
+    else if (argc == 2) 
+    {
+        configFile = argv[1];
+    } 
+    else 
+    {
+        std::cerr << "Usage: ./webserv [config_file.conf]" << std::endl;
+        return 1;
+    }
+    if (!hasConfExtension(configFile)) 
+    {
+        std::cerr << "Error: Configuration file MUST end with '.conf' (e.g., webserv.conf)" << std::endl;
         return 1;
     }
     try
     {
         ConfigParser parser;
-        parser.parse(argv[1]);
+        parser.parse(configFile);
+        
         std::vector<ServerConfig> servers = parser.getServers();
         std::cout << "Successfully parsed config file!" << std::endl;
         std::cout << "Found " << servers.size() << " servers." << std::endl;
+        
         if (!servers.empty())
         {
             SocketManager manager;
@@ -28,33 +65,15 @@ int main(int argc, char **argv)
             EpollManager epollManager;
             for (size_t i = 0; i < fds.size(); ++i)
                 epollManager.ctrl(fds[i], EPOLLIN, EPOLL_CTL_ADD);
+                
             Event event;
             event.run(manager, epollManager);
-            // manager.cleanup();
         }
-    } catch (const std::exception &e)
+    } 
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
     }
+    
+    return 0;
 }
-// =======
-//     try
-//     {
-//         ConfigParser parser;
-//         parser.parse(argv[1]);
-//         std::vector<ServerConfig> servers = parser.getServers();
-//         std::cout << "Successfully parsed config file!" << std::endl;
-//         std::cout << "Found " << servers.size() << " servers." << std::endl;
-//         if (!servers.empty())
-//         {
-            
-//             std::cout << "Server 1 listening on port: " << servers[0].port << std::endl;
-//             std::cout << "Server 1 root: " << servers[0].locations[0].root << std::endl;
-//         }
-//     } catch (const std::exception &e)
-//     {
-//         std::cerr << e.what() << std::endl;
-//     }
-
-//     return 0;
-// }
